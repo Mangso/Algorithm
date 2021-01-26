40f3f730a3dd09204c6d99911b674c54cee99f7b
Mangso <akfls_thakd@naver.com>
2021-01-26 18:00:21 +0900

    [Add] 2748_피보나치 수 2

---
+++ foscar_study/week4/2748.cpp
@@ -0,0 +1,25 @@
+// 피보나치 수 2
+
+#include <iostream>
+
+using namespace std;
+
+long long arr[91];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    
+    cout << arr[n];
+
+    return 0;
+}
