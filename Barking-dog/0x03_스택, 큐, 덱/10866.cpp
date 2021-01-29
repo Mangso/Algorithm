9f14f3dd5876800481e6b3900922ba5076132f23
Mangso <akfls_thakd@naver.com>
2021-01-29 17:18:17 +0900

    [Add] 10866_덱

---
+++ Barking-dog/0x03_스택, 큐, 덱/10866.cpp
@@ -0,0 +1,84 @@
+// 덱
+
+#include <iostream>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+deque<int> dq;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, num;
+    cin >> n;
+
+    while (n--)
+    {
+        string str;
+        cin >> str;
+
+        if (str == "push_back")
+        {
+            cin >> num;
+            dq.push_back(num);
+        }
+        else if (str == "push_front")
+        {
+            cin >> num;
+            dq.push_front(num);
+        }
+        else if (str == "pop_front")
+        {
+
+            if (dq.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << dq.front() << '\n';
+                dq.pop_front();
+            }
+        }
+        else if (str == "pop_back")
+        {
+
+            if (dq.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << dq.back() << '\n';
+                dq.pop_back();
+            }
+        }
+        else if (str == "size")
+        {
+            cout << dq.size() << '\n';
+        }
+        else if (str == "empty")
+        {
+            cout << dq.empty() << '\n';
+        }
+        else if (str == "front")
+        {
+            if (dq.empty())
+                cout << -1 << '\n';
+            else
+                cout << dq.front() << '\n';
+        }
+        else if (str == "back")
+        {
+            if (dq.empty())
+                cout << -1 << '\n';
+            else
+                cout << dq.back() << '\n';
+        }
+    }
+    return 0;
+}
