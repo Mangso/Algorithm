ee039b2657b5418153e54d316c6f05e2e4a4a4c0
Mangso <akfls_thakd@naver.com>
2022-08-01 10:06:01 +0900

    좌표 정렬하기

---
+++ Barking-dog/0x0E_Sort_1/11650.cpp
@@ -0,0 +1,39 @@
+// 좌표 정렬하기
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool Compare(const pair<int, int>& a, const pair<int, int>& b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
+    }
+
+    return a.first < b.first;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> coord;
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        coord.push_back({x, y});
+    }
+
+    sort(coord.begin(), coord.end(), Compare);
+
+    for (const auto& ele : coord) {
+        cout << ele.first << " " << ele.second << '\n';
+    }
+
+    return 0;
+}
