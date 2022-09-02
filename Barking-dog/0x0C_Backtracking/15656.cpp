314f883670f928346c49f0eead04f333a0697dc8
Mangso <akfls_thakd@naver.com>
2022-09-02 14:02:48 +0900

    N과 M(7)

---
+++ Barking-dog/0x0C_Backtracking/15656.cpp
@@ -0,0 +1,40 @@
+// N과 M(5)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+vector<int> V(10);
+vector<int> answer(10);
+vector<bool> isused(10, false);
+
+void func(int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            cout << answer[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        answer[depth] = V[i];
+        func(depth + 1);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> V[i];
+    }
+    sort(V.begin(), V.begin() + N);
+    func(0);
+
+    return 0;
+}
